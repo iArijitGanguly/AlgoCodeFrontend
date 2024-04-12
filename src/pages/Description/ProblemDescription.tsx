03cef3328fafa811a159c47384517075e02abacf
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-12 21:06:30 +0530

    Feat: Integrated react ace editor amd react markdown

---
+++ src/pages/Description/ProblemDescription.tsx
@@ -0,0 +1,88 @@
+import 'ace-builds/src-noconflict/ace';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/ext-language_tools';
+
+import DOMPurify from 'dompurify';
+import { useState } from 'react';
+import AceEditor from 'react-ace';
+import Markdown from 'react-markdown';
+
+function Description({ descriptionText }) {
+  const [activeTab, setActiveTab] = useState('statement');
+  const [leftWidth, setLeftWidth] = useState(50);
+  const [isDragging, setIsDragging] = useState(false);
+  const sanitizedMarkdown = DOMPurify.sanitize(descriptionText);
+
+  const startDragging = () => {
+    setIsDragging(true);
+  };
+
+  const stopDragging = () => {
+    setIsDragging(false);
+  };
+
+  const onDrag = (e) => {
+    if (!isDragging) return;
+
+    const newLeftWidth = (e.clientX / window.innerWidth) * 100;
+    if (newLeftWidth > 10 && newLeftWidth < 90) {
+      setLeftWidth(newLeftWidth);
+    }
+  };
+
+  return (
+    <div
+      className="container flex w-full h-screen"
+      onMouseMove={onDrag}
+      onMouseUp={stopDragging}
+    >
+      <div
+        className="leftPanel h-full overflow-auto"
+        style={{ width: `${leftWidth}%` }}
+      >
+        <div className="tabs">
+          <button onClick={() => setActiveTab('statement')}>
+            Problem Statement
+          </button>
+          <button onClick={() => setActiveTab('editorial')}>Editorial</button>
+          <button onClick={() => setActiveTab('submission')}>
+            Submissions
+          </button>
+        </div>
+
+        <div className="markdownViewer p-5 basis-1/2">
+          <Markdown>{sanitizedMarkdown}</Markdown>
+        </div>
+      </div>
+
+      <div
+        className="divider cursor-col-resize w-2 h-full bg-slate-200"
+        onMouseDown={startDragging}
+      ></div>
+
+      <div
+        className="rightPanel h-full overflow-auto"
+        style={{ width: `${100 - leftWidth}%` }}
+      >
+        <div className="editorContainer">
+          <AceEditor
+            mode="javascript"
+            theme="monokai"
+            name="codeEditor"
+            className="editor"
+            setOptions={{
+              enableBasicAutocompletion: true,
+              enableLiveAutocompletion: true,
+              enableSnippets: true,
+              showLineNumbers: true,
+              fontSize: 16,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Description;
